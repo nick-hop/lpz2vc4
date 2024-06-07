7bd772ffbcabaca738e3f725af9b520e5eda0f4b
NickHopkins <nhopkins@dgitech.com>
2024-06-07 13:08:55 -0400

    cleanup of print statements

--- lpz2VC4.py
+++ lpz2VC4.py
@@ -1,18 +1,28 @@
+"""
+lpz2VC4.py
+
+This script is used to monitor a directory and upload new files to the VC4 server. 
+It uses PyQt5 for the GUI and requests for the HTTP requests.
+
+Author: Nick Hopkins
+Creation Date: 06/06/2024
+"""
+_software_version = "v1.0.1"
+
+
 import os
 import sys
 import time
 import requests
 import warnings
 import datetime
-from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QVBoxLayout, \
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLineEdit, \
                             QPushButton, QComboBox, QLabel, QFileDialog, \
-                            QTextEdit, QLineEdit
+                            QTextEdit
 from PyQt5.QtCore import QThread, pyqtSignal, Qt
 
-# Suppressing the warning
 warnings.filterwarnings("ignore", message="Unverified HTTPS request")
 
-# Function to query the VC4 server for all programs
 def get_all_programs():
     """
     Function to query the VC4 server for all programs.
@@ -22,21 +32,15 @@ def get_all_programs():
         'accept': 'application/json',
         'Authorization': auth_token
     }
-    response = requests.get(api_endpoint, headers=headers, verify=False)  # Disabling SSL verification
+    response = requests.get(api_endpoint, headers=headers, verify=False) 
     if response.status_code == 200:
         data = response.json()
-        # Extract programs from the nested dictionary
         programs = data.get('Device', {}).get('Programs', {}).get('ProgramLibrary', {})
-        # Ensure programs is a dictionary of dictionaries
         if isinstance(programs, dict) and all(isinstance(program, dict) for program in programs.values()):
-            print("Retrieved Programs:")
-            print(programs)  # Print all programs
-            return list(programs.values())  # Return programs as a list of dictionaries
+            return list(programs.values()) 
         else:
-            print("Programs data is not in the expected format.")
             return None
     else:
-        print(f"Failed to retrieve programs. Status code: {response.status_code}")
         return None
 
 class MonitorThread(QThread):
@@ -46,9 +50,6 @@ class MonitorThread(QThread):
     upload_time_signal = pyqtSignal(str)
 
     def __init__(self, directory, programs, combo_index, upload_log,api_endpoint):  
-        """
-        Initialize the MonitorThread.
-        """
         super().__init__()
         self.directory = directory
         self.programs = programs
@@ -57,38 +58,31 @@ class MonitorThread(QThread):
         self.upload_log = upload_log
         self.api_endpoint = api_endpoint
     
-    # Upload the file to the VC4 server
     def upload_file(self, file_path, file_name, program_id, friendly_name):
         """
         Upload the file to the VC4 server.
         Returns a success message if the upload was successful, an error message otherwise.
         """
-        # Construct the request headers
         headers = {
             'Authorization': auth_token
         }
         
-        # Construct the request payload
         payload = {
             'ProgramId': program_id,
             'StartNow': 'true'
         }
         
-        # Construct the file data
         files = {
             'AppFile': (file_name, open(os.path.join(file_path, file_name), 'rb'))
         }
         
-        # Send the request
         response = requests.put(self.api_endpoint, headers=headers, data=payload, files=files, verify=False)
-        
-        # Check the response status code
+
         if response.status_code == 200:
             return "File uploaded successfully."
         else:
             return f"Failed to upload file. Status code: {response.status_code}"
 
-        # Helper function to get the latest file in the directory
     def get_LPZ_file(self, directory):
         """
         Get the latest .lpz file in the directory.
@@ -99,7 +93,6 @@ class MonitorThread(QThread):
             return None
         LPZ_file = max(files, key=lambda x: os.path.getmtime(os.path.join(directory, x)))
         LPZ_file = LPZ_file.replace("/","\\")
-        print("LPZfile: ",LPZ_file)
         return LPZ_file
 
     def run(self):
@@ -113,17 +106,15 @@ class MonitorThread(QThread):
                 LPZ_file_time = os.path.getmtime(LPZ_file)
                 if LPZ_file_time > last_uploaded_time:
                     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    print(f"\n\n[{current_time}] New file found: {os.path.basename(LPZ_file)}.")
                     chosen_program = self.programs[self.combo_index]
                     response = self.upload_file(self.directory, LPZ_file, chosen_program["ProgramId"], chosen_program["FriendlyName"])
                     if "Failed to upload file" in response:
-                        print(response)  # Print error message
+                        pass 
                     else:
-                        print("File uploaded successfully.")
                         last_uploaded_time = LPZ_file_time
                         self.upload_time_signal.emit(current_time)
             else:
-                print("No .LPZ files found in the directory.")
+                pass
             time.sleep(1)  
 
     def stop(self):
@@ -137,9 +128,6 @@ class AuthIpDialog(QDialog):
     Dialog for entering the auth token and IP address.
     """
     def __init__(self, auth_token=None, ip_address=None):
-        """
-        Initialize the AuthIpDialog.
-        """
         super().__init__()
 
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
@@ -181,15 +169,11 @@ class AuthIpDialog(QDialog):
         """
         return self.ip_address_field.text()   
 
-# Main function to monitor the directory and upload new files
 class MainWindow(QDialog):
     """
     Main window for the application.
     """
     def __init__(self, programs,auth_token=None, api_endpoint=None):
-        """
-        Initialize the MainWindow.
-        """
         super().__init__()
 
         self.setWindowTitle("LPZ-2-VC4")
@@ -219,7 +203,7 @@ class MainWindow(QDialog):
         self.upload_log = QTextEdit()  
         self.upload_log.setReadOnly(True)  
 
-        self.version = QLabel("v1.0.0") 
+        self.version = QLabel(_software_version) 
 
         self.layout.addWidget(self.program_label)
         self.layout.addWidget(self.program_combo)
@@ -229,7 +213,6 @@ class MainWindow(QDialog):
         self.layout.addWidget(self.monitor_button)
         self.layout.addWidget(self.upload_log)  
         self.layout.addWidget(self.version) 
-
         self.setLayout(self.layout)
 
     def select_directory(self):
@@ -238,7 +221,6 @@ class MainWindow(QDialog):
         """
         self.directory = QFileDialog.getExistingDirectory(self, "Select Directory")
         if self.directory:
-            print(f"Selected directory: {self.directory}")
             self.selected_directory_label.setText(f"Selected directory: {self.directory}")   
 
     def enable_monitor(self):
@@ -252,7 +234,6 @@ class MainWindow(QDialog):
                 self.monitor_button.setText("Enable Monitor")
                 self.program_combo.setEnabled(True)
                 self.directory_button.setEnabled(True)
-                print("Monitoring disabled.")
             else:
                 self.monitoring = True
                 self.monitor_button.setText("Disable Monitor")
@@ -262,7 +243,7 @@ class MainWindow(QDialog):
                 self.monitor_thread.upload_time_signal.connect(self.update_upload_log)
                 self.monitor_thread.start()
         else:
-            print("No directory selected.")
+            pass
 
     def update_upload_log(self, upload_time):
         """
@@ -282,13 +263,11 @@ if __name__ == "__main__":
     os.makedirs(lpz2vc4_dir, exist_ok=True)
 
     auth_token_file = os.path.join(lpz2vc4_dir, 'settings.conf')
-
     auth_token = None
     ip_address = None
     if os.path.exists(auth_token_file):
         with open(auth_token_file, 'r') as file:
             auth_token, ip_address = file.read().strip().split(',')
-
     
     auth_ip_dialog = AuthIpDialog(auth_token, ip_address)
     if auth_ip_dialog.exec_() == QDialog.Accepted:
@@ -300,7 +279,6 @@ if __name__ == "__main__":
         sys.exit()
     
     api_endpoint = f"https://{ip_address}/VirtualControl/config/api/ProgramLibrary"
-
     programs = get_all_programs()
     if programs:
         window = MainWindow(programs, auth_token, api_endpoint)
